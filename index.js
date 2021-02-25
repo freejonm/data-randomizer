a81ff32f0307315ce12ab28de15e3e67dd7501b7
Jon Free <freejonm@gmail.com>
2021-02-25 15:48:21 -0500

    randomize function works

--- index.js
+++ index.js
@@ -11,20 +11,68 @@ const connection = mysql.createConnection({
     database: 'data_to_rand'
 })
 
-const readData = () => {
-    connection.query('SELECT * FROM initial_data', (err, res) => {
-        if (err) throw err;
+// const readData = () => {
+//     connection.query('SELECT * FROM initial_data', (err, res) => {
+//         if (err) throw err;
 
-        console.table(res);
-        connection.end();
-    })
+//         console.table(res);
+//         connection.end();
+//     })
+// }
+
+const randomize = () => {
+
+
+    let min = 1;
+    let max = 10;
+    let stop = 4;
+
+    let numbers = [];
+
+    for(let i = 0; i < stop; i++){
+        let n = Math.floor(Math.random() * max) + min;
+
+        let check = numbers.includes(n);
+
+        if (check === false){
+            numbers.push(n);
+        } else {
+            while (check === true){
+                n = Math.floor(Math.random() * max) + min;
+                check = numbers.includes(n);
+                if(check === false){
+                    numbers.push(n);
+                }
+            }
+        }
+    
+        
+    }
+
+    console.log(numbers);
+
+    
+    // connection.query('SELECT * FROM initial_data WHERE ?',
+    // {
+    //     id: 
+    // }, 
+    // (err, res) => {
+    //     if (err) throw err;
+        
+    //     console.table(res);
+    //     connection.end();
+    // })
 }
 
 
 
-// Connect to the DB
-connection.connect((err) => {
-    if (err) throw err;
-    console.log(`connected as id ${connection.threadId}\n`);
-    readData();
-  });
+randomize();
+
+
+// // Connect to the DB
+// connection.connect((err) => {
+//     if (err) throw err;
+//     console.log(`connected as id ${connection.threadId}\n`);
+//     // readData();
+//     randomize();
+//   });
