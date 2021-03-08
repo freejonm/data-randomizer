9f4293fec0579aa6500c73faa6f189713c9636e2
Jon Free <freejonm@gmail.com>
2021-03-08 10:55:31 -0500

    adding readme

--- script.js
+++ script.js
@@ -3,6 +3,10 @@ const fs = require('fs');
 const csv = require('fast-csv');
 const ws = fs.createWriteStream("randomized_data.csv");
 
+// Edit the lines below so the values match the desired number of random samples.
+
+const limit = 5;
+
 const connection = mysql.createConnection({
     host: 'localhost',
 
@@ -10,19 +14,21 @@ const connection = mysql.createConnection({
 
     user: 'root',
 
+    // Be sure to change the password below to your MySQL root password. 
     password: 'a1b2c3d4',
+
     database: 'data_to_rand'
 })
 
 
 const getRandom = () => {    
-connection.query('SELECT * FROM initial_data ORDER BY RAND() LIMIT 5;',
+connection.query(`SELECT * FROM initial_data ORDER BY RAND() LIMIT ${limit};`,
         (err, res) => {
         if (err) throw err;
-        
-        console.table(res);
 
-        csv.write(res, { headers: true}).on("finish", function(){console.log("File written!");}).pipe(ws);
+        csv.write(res, { headers: true}).on("finish", function(){console.log("CSV file written with the data displayed here.");}).pipe(ws);
+
+        console.table(res);
 
         connection.end();
     });
